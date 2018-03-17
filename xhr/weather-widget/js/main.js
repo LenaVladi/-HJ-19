360eceaa6bb96f6b304297a7f63524511b552f9f
Lena Vladi <lenavladi.web@ya.ru>
2018-03-18 01:40:34 +0300

    fixed reguest

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,7 +1,17 @@
+// const request = new XMLHttpRequest();
+// request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', false);
+// request.send();
+// if (request.status === 200) {
+//   const response = JSON.parse(request.responseText);
+//   setData(response);
+// }
+
 const request = new XMLHttpRequest();
-request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', false);
+request.addEventListener('load', display)
+request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather');
 request.send();
-if (request.status === 200) {
+
+function display() {
   const response = JSON.parse(request.responseText);
   setData(response);
-}
+};
