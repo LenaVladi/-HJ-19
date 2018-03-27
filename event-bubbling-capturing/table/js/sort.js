f61171409e50b4ad02199103d8b4079efd441273
Lena Vladi <lenavladi.web@ya.ru>
2018-03-27 20:29:19 +0300

    add function handleTableClick

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,14 @@
 'use strict';
 
+const popName = document.querySelector('.prop__name');
+
 function handleTableClick(event) {
-  
+  event.target.dataset.dir = event.currentTarget.dataset.dir ? 1 : -1;
+  let field = event.target.dataset.popName;
+  let direction = event.target.dataset.dir;
+  sortTable(field, direction);
 }
+
+Array.from(popName).forEach(th => {
+  th.addEventListener('click', handleTableClick);
+})
