2eafd775450c66b4ec558d7a33f2b63e74822ca6
Lena Vladi <lenavladi.web@ya.ru>
2018-03-27 17:19:40 +0300

    leafs tabs

--- html-document-structure/tabs/js/tabs.js
+++ html-document-structure/tabs/js/tabs.js
@@ -4,28 +4,42 @@ const tabsContent = document.getElementsByClassName('tabs-content')[0].children;
 const tabsNav = document.getElementsByClassName('tabs-nav')[0];
 const cloneTabsNav = tabsNav.cloneNode(true);
 tabsNav.removeChild(tabsNav.getElementsByTagName('li')[0]);
-let activeTabs = tabsContent[0].classList.add('ui-tabs-active');
-console.log(TABS, tabsContent, tabsNav);
+console.log(TABS, tabsContent, tabsNav, cloneTabsNav);
+
+Array.from(tabsContent).forEach(tab => tab.classList.toggle('hidden'));
+
+let activeTab = tabsContent[0].classList.remove('hidden');
+activeTab = tabsContent[0].classList.add('ui-tabs-active');
+
+
 
 for (let i = 0; i < tabsContent.length; i++) {
   let li = document.createElement('li');
   li.classList.add(tabsContent[i].getAttribute('data-tab-icon'));
   let a = document.createElement('a');
-  li.appendChild(a);
   a.classList.add('fa');
   a.textContent = tabsContent[i].getAttribute('data-tab-title');
+  li.appendChild(a);
   tabsNav.appendChild(li);
 }
 
 
 
 function tabsShow(event) {
-  tabsNav.appendChild()
-  let currentTabs = tabsContent.nextElementSibling;
-  currentTabs.classList.add('ui-tabs-active');
+  event.preventDefault();
+  console.log(event.target.textContent);
+  Array.from(tabsContent).forEach(tab => {
+    console.log(tab.dataset.tabTitle);
+    tab.classList.add('hidden');
+    if (event.target.textContent === tab.dataset.tabTitle) {
+      tab.classList.remove('hidden');
+      tab.classList.add('ui-tabs-active');
+    }
+  });
+
 
 
 }
 
-Array.from(tabsContent).forEach(tab => tab
+Array.from(tabsNav.children).forEach(tab => tab
   .addEventListener('click', tabsShow));
