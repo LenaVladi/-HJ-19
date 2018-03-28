3e439ace904bb521af8be37e81e4dbac12b71672
Lena Vladi <lenavladi.web@ya.ru>
2018-03-28 22:59:44 +0300

    function addToCard

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,15 @@
 'use strict';
+const itemsList = document.querySelector('.items-list');
+
+function btn(event) {
+  console.log(event.target);
+  if (event.target.className === document.querySelector('.add-to-cart').className) {
+    let item = {
+      title : event.target.dataset.title,
+      price : event.target.dataset.price
+    };
+    addToCart(item);
+  }
+}
+
+itemsList.addEventListener('click', btn);
