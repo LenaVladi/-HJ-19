f62c8feae750ce9655323d4c31a01eec294a8c8c
Lena Vladi <lenavladi.web@ya.ru>
2018-03-30 11:55:44 +0300

    backlight piano

--- event-object/piano/js/script.js
+++ event-object/piano/js/script.js
@@ -3,15 +3,21 @@ const setPiano = document.getElementsByClassName('set')[0];
 const audios = setPiano.getElementsByTagName('audio');
 const liAudio = setPiano.getElementsByTagName('li');
 const sounds = ['first.mp3', 'second.mp3', 'third.mp3', 'fourth.mp3', 'fifth.mp3'];
-//const FolderSounds = ['sounds/middle/', 'sounds/lower/', 'sounds/higher/'];
 const audiosArray = Array.from(audios);
 
 function modulePiano(event) {
   if (event.shiftKey) {
+    setPiano.classList.remove('higher');
+    setPiano.classList.add('lower');
     return `sounds/lower/`;
   } else if(event.altKey) {
+    setPiano.classList.remove('lower');
+    setPiano.classList.add('higher');
     return `sounds/higher/`;
   } else {
+    setPiano.classList.remove('higher');
+    setPiano.classList.remove('lower');
+    setPiano.classList.add('middle');
     return `sounds/middle/`;
   }
 }
