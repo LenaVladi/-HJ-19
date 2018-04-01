7fd59012d768888e89434c68a29805300b2672b1
Lena Vladi <lenavladi.web@ya.ru>
2018-04-01 21:32:58 +0300

    fixed handleTableClick

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,14 +1,11 @@
 'use strict';
-
 const popName = document.querySelector('.prop__name');
 
 function handleTableClick(event) {
-  event.target.dataset.dir = event.currentTarget.dataset.dir ? 1 : -1;
-  let field = event.target.dataset.popName;
-  let direction = event.target.dataset.dir;
-  sortTable(field, direction);
+  if (event.target.classList.contains('prop__name')) {
+    event.target.dataset.dir = event.currentTarget.dataset.dir ? 1 : -1;
+    let field = event.target.dataset.popName;
+    let direction = event.target.dataset.dir;
+    sortTable(field, direction);
+  }  
 }
-
-Array.from(popName).forEach(th => {
-  th.addEventListener('click', handleTableClick);
-})
