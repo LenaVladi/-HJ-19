6c85e213755d577373c14a75256ac7b72f35555c
Lena Vladi <lenavladi.web@ya.ru>
2018-04-01 22:04:42 +0300

    implemented personal counter, localStorage

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,24 @@
 'use strict';
+const counter = document.getElementById('counter');
+const increment = document.getElementById('increment');
+const decrement = document.getElementById('decrement');
+const reset = document.getElementById('reset');
+
+console.log(counter);
+counter.textContent = '0';
+
+increment.addEventListener('click', () => {
+    localStorage.setItem('counter', counter.textContent = parseInt(counter.textContent) + 1);
+});
+decrement.addEventListener('click', () => {
+    if (parseInt(counter.textContent) === 0) {
+        return;
+    } else {
+        localStorage.setItem('counter',counter.textContent = parseInt(counter.textContent) - 1);
+    }  
+});
+reset.addEventListener('click', () => {
+    localStorage.setItem('counter',counter.textContent = '0');
+});
+
+counter.textContent = localStorage.counter;
