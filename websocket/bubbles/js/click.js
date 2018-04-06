3b6b0f42e2b68fb9393829762e4111b6170bea61
Lena Vladi <lenavladi.web@ya.ru>
2018-04-06 23:48:56 +0300

    click

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,15 @@
 'use strinct';
+
+const connection = new WebSocket('ws://neto-api.herokuapp.com/mouse');
+
+showBubbles(connection)
+
+document.addEventListener('click', (event) => {
+    let click = {
+        x : event.x,
+        y : event.y
+    }
+    connection.send(JSON.stringify(click));
+});
+
+
