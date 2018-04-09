d67f5b749db32a89fd94ef5776a15598aa10c655
Lena Vladi <lenavladi.web@ya.ru>
2018-04-10 00:30:37 +0300

    twitter user profile widget

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function loadData(jsonp) {
+    return new Promise((done, fail) => {
+        //window.alena = done;
+        let script = document.createElement('script');
+        script.src = jsonp;
+        document.body.appendChild(script);
+    });
+}; 
+  
+
+function callback(twit) {
+    //console.log(twit);
+    const pic = document.querySelector('img[data-pic]');
+    pic.src = twit.pic;
+    const username = document.querySelector('h3[data-username]');
+    username.textContent = twit.username;
+    const wallpaper = document.querySelector('img[data-wallpaper]');
+    wallpaper.src = twit.wallpaper;
+    const description = document.querySelector('p[data-description]');
+    description.textContent = twit.description;
+    const tweets = document.querySelector('output[data-tweets]');
+    tweets.innerHTML = twit.tweets;
+    const followers = document.querySelector('output[data-followers]');
+    followers.textContent = twit.followers;
+    const following = document.querySelector('output[data-following]');
+    following.textContent = twit.following;
+}
+
+
+loadData('https://neto-api.herokuapp.com/twitter/jsonp')
+.then(res => showTwit(res)).catch(er => console.log(er));
