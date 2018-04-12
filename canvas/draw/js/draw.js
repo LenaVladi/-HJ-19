b74bdd26c08db792c79404a735cc72fc9bd25f39
Lena Vladi <lenavladi.web@ya.ru>
2018-04-12 21:41:43 +0300

    height/width canvas

--- canvas/draw/js/draw.js
+++ canvas/draw/js/draw.js
@@ -1,1 +1,18 @@
-'use strict';
+'use strict'
+const canvas = document.getElementById('draw');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener('resize', () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;  
+});
+
+canvas.addEventListener('dblclick', () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+const line = [],
