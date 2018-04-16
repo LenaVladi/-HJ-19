66de4e6002d353a3d41bdc9e879918437ddb721e
Lena Vladi <lenavladi.web@ya.ru>
2018-04-17 00:54:19 +0300

    canvas.toBlob()

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+window.editor.addEventListener('update', event => {
+  console.log(event);
+  event.canvas.toBlob(status => ws.send(status));
+});
