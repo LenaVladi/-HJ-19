7ac3fc7b0c8b79705a295cf5e4cf4313cd8291ab
Lena Vladi <lenavladi.web@ya.ru>
2018-04-17 01:19:42 +0300

    web socket request

--- comet/random/js/websocket.js
+++ comet/random/js/websocket.js
@@ -1,1 +1,24 @@
 'use strict';
+const websocket = document.querySelector('.websocket');
+const cardWebsocet = websocket.getElementsByTagName('div');
+
+const ws = new WebSocket('wss://neto-api.herokuapp.com/comet/websocket');
+
+function webSocketRequest() {
+  const ws = new WebSocket('wss://neto-api.herokuapp.com/comet/websocket');
+  ws.addEventListener('message', event => {
+    const data = JSON.parse(event.data);
+    console.log(event, data);
+    Array.from(cardWebsocet).forEach(el => {
+      console.log(el.textContent);
+      //el.classList.remove('flip-it');
+      if (el.textContent === data) {
+        el.classList.add('flip-it');
+      }
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  webSocketRequest();
+});
