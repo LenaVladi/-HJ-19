16667aa121b29c211e196db15aeb3cbccda28580
Lena Vladi <lenavladi.web@ya.ru>
2018-04-25 02:18:16 +0300

    fixed counter

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -5,20 +5,26 @@ const decrement = document.getElementById('decrement');
 const reset = document.getElementById('reset');
 
 console.log(counter);
-counter.textContent = '0';
+counter.textContent = localStorage.count;
+if(!localStorage.count){
+    localStorage.count = 0
+}
+
 
 increment.addEventListener('click', () => {
-    localStorage.setItem('counter', counter.textContent = parseInt(counter.textContent) + 1);
+    localStorage.count++;
+    counter.textContent = localStorage.count;
 });
 decrement.addEventListener('click', () => {
     if (parseInt(counter.textContent) === 0) {
         return;
-    } else {
-        localStorage.setItem('counter',counter.textContent = parseInt(counter.textContent) - 1);
+    } else if (parseInt(counter.textContent) > 0) {
+        localStorage.count--;
+        counter.textContent = localStorage.count;
     }  
 });
 reset.addEventListener('click', () => {
-    localStorage.setItem('counter',counter.textContent = '0');
+    localStorage.count = 0;
+    counter.textContent = localStorage.count;
+    //localStorage.setItem('counter',counter.textContent = '0');
 });
-
-counter.textContent = localStorage.counter;
