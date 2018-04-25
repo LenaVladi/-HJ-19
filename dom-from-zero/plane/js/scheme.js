5176461bf9acef22f8e09a18f3258f5920e77969
Lena Vladi <lenavladi.web@ya.ru>
2018-04-26 00:22:19 +0300

    fixed typos, added function: update of schema cleaning schema, the counter seats

--- dom-from-zero/plane/js/scheme.js
+++ dom-from-zero/plane/js/scheme.js
@@ -1,5 +1,4 @@
 'use strict'
-
 const ACSelect = document.querySelector('#acSelect');
 const btnSeatMap = document.querySelector('#btnSeatMap');
 const btnSetFull = document.querySelector('#btnSetFull');
@@ -17,6 +16,7 @@ function requestScheme(event) {
   fetch(`https://neto-api.herokuapp.com/plane/${ACSelect.value}`)
     .then(res => res.json())
     .then(data => {
+      //console.log('data -->', data);
       totalPax.textContent = data.passengers;
       btnSetFull.disabled = false;
       btnSetEmpty.disabled = false;
@@ -25,21 +25,22 @@ function requestScheme(event) {
       showSeatMap(data);
 
       document.querySelectorAll('div.seat').forEach(seat => {
-        seat.addEventListener('click', clickOnSeat);
+        seat.addEventListener('click', counterSeat);
       })
     });
 }
 
 //html-шаблон рядов сидений в самолёте
-function createSeatingRow(seatRow) {
+function createSeating(seatRow) {
+  //console.log('seatRow -->', seatRow);
   return {
     tag: 'div',
     cls: ['row', 'seating-row', 'text-centr'],
     content: [
       {
-        tag: 'div', cls: ['col-xs-1', 'row-number'], // проверить шаблон места
+        tag: 'div', cls: ['col-xs-1', 'row-number'], 
         content: [
-          { tag: 'h2', content: saetRow.rowNum }
+          { tag: 'h2', content: seatRow.rowNum }
         ]
       },
       {
@@ -63,7 +64,7 @@ function createSeatingRow(seatRow) {
 }
 
 //создаем дом-элементы по шаблону
-function rowJsEngine(rows) {
+function seatingJsEngine(rows) {
   if ((typeof rows === 'string') || (typeof rows === 'number') || (rows === true)) {
     return document.createTextNode(rows);
   }
@@ -74,7 +75,7 @@ function rowJsEngine(rows) {
 
   if (Array.isArray(rows)) {
     return rows.reduce((fragment, elem) => {
-      fragment.appendChild(rowJsEngine(elem));
+      fragment.appendChild(seatingJsEngine(elem));
 
       return fragment;
     }, document.createDocumentFragment());
@@ -94,18 +95,18 @@ function rowJsEngine(rows) {
     });
   }
 
-  element.appendChild(rowJsEngine(rows.content));
+  element.appendChild(seatingJsEngine(rows.content));
 
   return element;
 }
 
-//показ пассажирских мест по клику
+//показ пассажирских мест, план самолёта
 function showSeatMap(seatMap) {
   const seats = document.createDocumentFragment();
 
   for (let i = 0; i < seatMap.scheme.length; i++) {
     let row1, row2;
-    console.log(seatMap.scheme, seatMap);
+
     if (seatMap.scheme[i] === 4) {
       row1 = [''].concat(seatMap['letters4'].slice(0, 2));
       row2 = seatMap['letters4'].slice(2, 4).concat(['']);
@@ -122,20 +123,85 @@ function showSeatMap(seatMap) {
       row1: row1,
       row2: row2
     };
-
-    seats.appendChild(rowJsEngine(createSeatingRow(rows)));
+    //console.log('rows -->', rows);
+    seats.appendChild(seatingJsEngine(createSeating(rows)));
   }
 
   seatMapDiv.textContent = '';
   seatMapDiv.appendChild(seats);
 }
 
+function clearSeats(event) {
+  event.preventDefault();
+  const seats = document.querySelectorAll('div.seat');
 
+  if (event.target === btnSetEmpty) {
+    //очистить
+    seats.forEach(seat => {
+      seat.classList.remove('adult');
+      seat.classList.remove('half');
+    });
 
+    totalAdult.textContent = 0;
+    totalHalf.textContent = 0;
 
+  } else if (event.target === btnSetFull) {
+    //заполнить
+    let className;
 
-document.addEventListener('DOMContentLoaded', selectedPlane);
-ACSelect.addEventListener('change', selectedPlane);
-btnSeatMap.addEventListener('click', requestScheme);
-btnSetFull.addEventListener('click', fillSeats);
-btnSetEmpty.addEventListener('click',fillSeats );
+    if (event.altKey) {
+      className = 'half';
+      totalAdult.textContent = 0;
+      totalHalf.textContent = totalPax.textContent;
+    } else {
+      className = 'adult';
+      totalAdult.textContent = totalPax.textContent;
+      totalHalf.textContent = 0;
+    }
+
+    seats.forEach(seat => {
+      seat.classList.remove('half');
+      seat.classList.remove('adult');
+      seat.classList.add(className);
+    });
+  }
+}
+
+function updatePlane() {
+  // обновление элементов
+  const selectedPlane =  ACSelect.querySelector(`[value="${ ACSelect.value}"]`);
+  seatMapTitle.textContent = `Airbus ${selectedPlane.textContent}`;
+  btnSetFull.disabled = true;
+  btnSetEmpty.disabled = true;
+  seatMapDiv.textContent = '';
+  seatMapDiv.appendChild(seatingJsEngine({ 
+    tag: 'h3', cls: 'text-center', content: 'Нажмите «Показать схему»' 
+  }));
+}
+
+function counterSeat(event) {
+  const targetSeat = (event.target.classList.contains('seat')) ? event.target : event.target.parentNode;
+
+  if (targetSeat.classList.contains('adult')) {
+    targetSeat.classList.remove('adult');
+    totalAdult.textContent--;
+
+  } else if (targetSeat.classList.contains('half')) {
+    targetSeat.classList.remove('half');
+    totalHalf.textContent--;
+
+  } else if (event.altKey) {
+    targetSeat.classList.add('half');
+    totalHalf.textContent++;
+
+  } else {
+    targetSeat.classList.add('adult');
+    totalAdult.textContent++;
+  }
+}
+
+document.addEventListener('DOMContentLoaded',  updatePlane);
+ACSelect.addEventListener('change',  updatePlane);
+btnSeatMap.addEventListener('click',requestScheme);
+btnSetFull.addEventListener('click', clearSeats);
+btnSetEmpty.addEventListener('click',clearSeats );
