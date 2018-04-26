a464a68b3e65c4900d1a4aea3142b0365b2e9a2c
Lena Vladi <lenavladi.web@ya.ru>
2018-04-26 14:40:07 +0300

    getNumber type sring -> number

--- comet/random/js/long-pooling.js
+++ comet/random/js/long-pooling.js
@@ -1,6 +1,8 @@
 'use strict';
 const longNumPooling = document.querySelector('.long-pooling').children;
 
+console.log(longNumPooling);
+
 function longPooling() {
   let xhr = new XMLHttpRequest();
   xhr.open(
@@ -10,17 +12,15 @@ function longPooling() {
   );
   xhr.send();
 
-  xhr.addEventListener('readystatechange', function() {
-    if (this.readyState != 4) return;
+  xhr.addEventListener('load', function() {
     const getNumber = xhr.responseText;
-    console.log(this);
 
     Array.from(longNumPooling).forEach(elem =>  {
       elem.classList.remove('flip-it');
     });
-    longNumPooling[getNumber].classList.add('flip-it');
+    longNumPooling[+getNumber].classList.add('flip-it');
 
-    longPooling();
+  longPooling();
   }); 
 }
 
